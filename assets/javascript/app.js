b40dd5608b4b23802f931dc1a33806d75744ee51
Alex H <08ahayden@gmail.com>
2017-12-18 22:19:04 -0500

    Timer, display, reset buttons and styling done. Need results

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,7 @@ var unanswered;
 //functions
 function game () {
   $("#startbutton").on("click", run);
-  $(".donebutton").on("click", stop);
+
 
 
 }
@@ -21,6 +21,7 @@ function game () {
 function stop (){
   clearInterval(intervalId);
   $(".Questions").empty();
+  $(".countdown").empty();
 }
 
 function run (){
@@ -28,7 +29,8 @@ function run (){
   number = 30;
   intervalId = setInterval(decrement, 1000);
   //there's gotta be a better, cleaner way to do this. Or mayber there isn't, idk it works right now so lets just go with it
-  $(".Questions").append("<h1> QUESTION 1 QUESTION 1 QUESTION 1 QUESTION 1 </h1>");
+  $(".Questionscontainer").append("<div class = Questions> </div>");
+  $(".Questions").append("<h1 class = questionheader> QUESTION 1 QUESTION 1 QUESTION 1 QUESTION 1 </h1>");
   $(".Questions").append("<form id=Q1>");
   $(".Questions").append("<input type= radio name=question1option value=answer> Answer1");
   $(".Questions").append("<input type= radio name=question1option value=answer> Answer2");
@@ -36,7 +38,7 @@ function run (){
   $(".Questions").append("<input type= radio name=question1option value=answer> Answer4");
   $(".Questions").append("</form>");
 
-  $(".Questions").append("<h1> QUESTION2 QUESTION 2 QUESTION 2 QUESTION 2 </h1>");
+  $(".Questions").append("<h1 class = questionheader> QUESTION2 QUESTION 2 QUESTION 2 QUESTION 2 </h1>");
   $(".Questions").append("<form id=Q2>");
   $(".Questions").append("<input type= radio name=question2option value=answer> Answer1");
   $(".Questions").append("<input type= radio name=question2option value=answer> Answer2");
@@ -44,48 +46,50 @@ function run (){
   $(".Questions").append("<input type= radio name=question2option value=answer> Answer4");
   $(".Questions").append("</form>");
 
-  $(".Questions").append("<h1> QUESTION 3 QUESTION 3 QUESTION 3 QUESTION 3 </h1>");
+  $(".Questions").append("<h1 class = questionheader> QUESTION 3 QUESTION 3 QUESTION 3 QUESTION 3 </h1>");
   $(".Questions").append("<form id=Q3></form>");
   $(".Questions").append("<input type= radio name=question3option value=answer> Answer1");
   $(".Questions").append("<input type= radio name=question3option value=answer> Answer2");
   $(".Questions").append("<input type= radio name=question3option value=answer> Answer3");
   $(".Questions").append("<input type= radio name=question3option value=answer> Answer4");
 
-  $(".Questions").append("<h1> QUESTION 4 QUESTION 4 QUESTION 4 QUESTION 4 </h1>");
+  $(".Questions").append("<h1 class = questionheader> QUESTION 4 QUESTION 4 QUESTION 4 QUESTION 4 </h1>");
   $(".Questions").append("<form id=Q4></form>");
   $(".Questions").append("<input type= radio name=question4option value=answer> Answer1");
   $(".Questions").append("<input type= radio name=question4option value=answer> Answer2");
   $(".Questions").append("<input type= radio name=question4option value=answer> Answer3");
   $(".Questions").append("<input type= radio name=question4option value=answer> Answer4");
 
-  $(".Questions").append("<h1> QUESTION 5 QUESTION 5 QUESTION 5 QUESTION 5 </h1>");
+  $(".Questions").append("<h1 class = questionheader> QUESTION 5 QUESTION 5 QUESTION 5 QUESTION 5 </h1>");
   $(".Questions").append("<form id=Q5></form>");
   $(".Questions").append("<input type= radio name=question5option value=answer> Answer1");
   $(".Questions").append("<input type= radio name=question5option value=answer> Answer2");
   $(".Questions").append("<input type= radio name=question5option value=answer> Answer3");
   $(".Questions").append("<input type= radio name=question5option value=answer> Answer4");
 
-  $(".Questions").append("<h1> QUESTION 6 QUESTION 6 QUESTION 6 QUESTION 6 </h1>");
+  $(".Questions").append("<h1 class = questionheader> QUESTION 6 QUESTION 6 QUESTION 6 QUESTION 6 </h1>");
   $(".Questions").append("<form id=Q6></form>");
   $(".Questions").append("<input type= radio name=question6option value=answer> Answer1");
   $(".Questions").append("<input type= radio name=question6option value=answer> Answer2");
   $(".Questions").append("<input type= radio name=question6option value=answer> Answer3");
   $(".Questions").append("<input type= radio name=question6option value=answer> Answer4");
 
-  $(".Questions").append("<h1> QUESTION 7 QUESTION 7 QUESTION 7 QUESTION 7 </h1>");
+  $(".Questions").append("<h1 class = questionheader> QUESTION 7 QUESTION 7 QUESTION 7 QUESTION 7 </h1>");
   $(".Questions").append("<form id=Q7></form>");
   $(".Questions").append("<input type= radio name=question7option value=answer> Answer1");
   $(".Questions").append("<input type= radio name=question7option value=answer> Answer2");
   $(".Questions").append("<input type= radio name=question7option value=answer> Answer3");
   $(".Questions").append("<input type= radio name=question7option value=answer> Answer4");
-  $(".Questions").append("<h1> QUESTION 8 QUESTION 8 QUESTION 8 QUESTION 8 </h1>");
-  $(".Questions").append("<form id=Q8></form>");
-  $(".Questions").append("<input type= radio name=question8option value=answer> Answer1");
-  $(".Questions").append("<input type= radio name=question8option value=answer> Answer2");
-  $(".Questions").append("<input type= radio name=question8option value=answer> Answer3");
-  $(".Questions").append("<input type= radio name=question8option value=answer> Answer4 <br>");
 
-  $(".Questions").append("<input type=submit value=Done! class= donebutton>")
+  $(".Questions").append("<h1 class = questionheader> QUESTION 8 QUESTION 8 QUESTION 8 QUESTION 8 </h1>");
+  $(".Questions").append("<form id=Q8></form>");
+  $(".Questions").append("<input type= radio name=question8option1 value=answer> Answer1");
+  $(".Questions").append("<input type= radio name=question8option2 value=answer> Answer2");
+  $(".Questions").append("<input type= radio name=question8option3 value=answer> Answer3");
+  $(".Questions").append("<input type= radio name=question8option4 value=answer> Answer4 <br>");
+  $(".Questions").append("<pre id=log></pre>");
+
+  $(".Questions").append("<input type=submit class=submitbutton form= Q1 Q2 Q3 Q4 Q5 Q6 Q7 Q8 id=donebutton value=Done! >")
 
 }
 function decrement (){
@@ -94,11 +98,21 @@ function decrement (){
   if (number == 0){
     stop();
     $(".Questions").empty();
+    $(".Questionscontainer").empty();
     document.getElementById('startbutton').style.display="block";
 
   }
 }
 
+function answerchecker () {
+  
+
+}
+
+function stopgame () {
+  $("#donebutton").on("click", stop);
+  stop();
+}
 
 
 
@@ -106,3 +120,4 @@ function decrement (){
 
 //calls
 game();
+stopgame();
