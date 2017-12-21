25b320aba596b4722c730f660ce08ec96f132311
Alex H <08ahayden@gmail.com>
2017-12-21 18:12:35 -0500

    ALL DONE

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,6 @@ var unanswered;
 function game () { // runs the game
   $("#startbutton").on("click", run);
 
-
 }
 
 function stop (){ //stops the game. This function is run when the user clicks the done button and when the timer runs out
@@ -25,69 +24,70 @@ function stop (){ //stops the game. This function is run when the user clicks th
 
 function run (){ //meat of the game
   document.getElementById('startbutton').style.display="none"; //hides the start button when the game starts
-  $(".Questionscontainer").empty(); //empties the container for restart
+  $(".Results").remove(); //empties the container for restart
+
   number = 30; //sets countdown number
   intervalId = setInterval(decrement, 1000); //starts countdown
   //there's gotta be a better, cleaner way to do this. Or mayber there isn't, idk it works right now so lets just go with it
   $(".Questionscontainer").append("<div class = Questions> </div>");
   $(".Questions").append("<form name=quiz method= post name= buttons id= quizid onsubmit=return false>");
-  $(".Questions").append("<h1 class = questionheader> QUESTION 1 QUESTION 1 QUESTION 1 QUESTION 1 </h1>");
+  $(".Questions").append("<h1 class = questionheader> What is the name of Santa's only female reindeer? </h1>");
 
-  $(".Questions").append("<input type= radio name=question1 value= wrong> Answer1");
-  $(".Questions").append("<input type= radio name=question1 value= correct> Correct");
-  $(".Questions").append("<input type= radio name=question1 value= wrong> Answer3");
-  $(".Questions").append("<input type= radio name=question1value= wrong> Answer4");
-
-
-  $(".Questions").append("<h1 class = questionheader> QUESTION2 QUESTION 2 QUESTION 2 QUESTION 2 </h1>");
-
-  $(".Questions").append("<input type= radio name=question2 value= correct> Correct");
-  $(".Questions").append("<input type= radio name=question2 value= wrong> Answer2");
-  $(".Questions").append("<input type= radio name=question2 value= wrong> Answer3");
-  $(".Questions").append("<input type= radio name=question2 value= wrong> Answer4");
+  $(".Questions").append("<input type= radio name=question1 value= wrong> Dasher");
+  $(".Questions").append("<input type= radio name=question1 value= correct> Vixen");
+  $(".Questions").append("<input type= radio name=question1 value= wrong> Dancer");
+  $(".Questions").append("<input type= radio name=question1value= wrong> Rudolph");
 
 
-  $(".Questions").append("<h1 class = questionheader> QUESTION 3 QUESTION 3 QUESTION 3 QUESTION 3 </h1>");
+  $(".Questions").append("<h1 class = questionheader> Which state was the last to declare Christmas an official holiday? </h1>");
 
-  $(".Questions").append("<input type= radio name=question3 value= wrong> Answer1");
-  $(".Questions").append("<input type= radio name=question3 value= wrong> Answer2");
-  $(".Questions").append("<input type= radio name=question3 value= wrong> Answer3");
-  $(".Questions").append("<input type= radio name=question3 value= correct> Correct");
+  $(".Questions").append("<input type= radio name=question2 value= correct> Oklahoma");
+  $(".Questions").append("<input type= radio name=question2 value= wrong> Washington");
+  $(".Questions").append("<input type= radio name=question2 value= wrong> Colorado");
+  $(".Questions").append("<input type= radio name=question2 value= wrong> Pennsylvania");
 
-  $(".Questions").append("<h1 class = questionheader> QUESTION 4 QUESTION 4 QUESTION 4 QUESTION 4 </h1>");
 
-  $(".Questions").append("<input type= radio name=question4 value= wrong> Answer1");
-  $(".Questions").append("<input type= radio name=question4 value= wrong> Answer2");
-  $(".Questions").append("<input type= radio name=question4 value= correct> Correct");
-  $(".Questions").append("<input type= radio name=question4 value= wrong> Answer4");
+  $(".Questions").append("<h1 class = questionheader> From which country is the original St. Nicholas? </h1>");
 
-  $(".Questions").append("<h1 class = questionheader> QUESTION 5 QUESTION 5 QUESTION 5 QUESTION 5 </h1>");
+  $(".Questions").append("<input type= radio name=question3 value= wrong> England");
+  $(".Questions").append("<input type= radio name=question3 value= wrong> Germany");
+  $(".Questions").append("<input type= radio name=question3 value= wrong> Austria");
+  $(".Questions").append("<input type= radio name=question3 value= correct> Turkey");
 
-  $(".Questions").append("<input type= radio name=question5 value= wrong> Answer1");
-  $(".Questions").append("<input type= radio name=question5 value= correct> Correct");
-  $(".Questions").append("<input type= radio name=question5 value= wrong> Answer3");
-  $(".Questions").append("<input type= radio name=question5 value= wrong> Answer4");
+  $(".Questions").append("<h1 class = questionheader> Where did the tradition of Christmas trees originate? </h1>");
 
-  $(".Questions").append("<h1 class = questionheader> QUESTION 6 QUESTION 6 QUESTION 6 QUESTION 6 </h1>");
+  $(".Questions").append("<input type= radio name=question4 value= wrong> Turkey");
+  $(".Questions").append("<input type= radio name=question4 value= wrong> France");
+  $(".Questions").append("<input type= radio name=question4 value= correct> Germany");
+  $(".Questions").append("<input type= radio name=question4 value= wrong> The Vatican");
 
-  $(".Questions").append("<input type= radio name=question6 value= correct> Correct");
-  $(".Questions").append("<input type= radio name=question6 value= wrong> Answer2");
-  $(".Questions").append("<input type= radio name=question6 value= wrong> Answer3");
-  $(".Questions").append("<input type= radio name=question6 value= wrong> Answer4");
+  $(".Questions").append("<h1 class = questionheader> What company is credited with designing Sanat's iconic red and white suit? </h1>");
 
-  $(".Questions").append("<h1 class = questionheader> QUESTION 7 QUESTION 7 QUESTION 7 QUESTION 7 </h1>");
+  $(".Questions").append("<input type= radio name=question5 value= wrong> A company didn't design that!");
+  $(".Questions").append("<input type= radio name=question5 value= correct> Coca Cola");
+  $(".Questions").append("<input type= radio name=question5 value= wrong> Vodafone");
+  $(".Questions").append("<input type= radio name=question5 value= wrong> Pepsi");
 
-  $(".Questions").append("<input type= radio name=question7 value= correct> Correct");
-  $(".Questions").append("<input type= radio name=question7 value= wrong> Answer2");
-  $(".Questions").append("<input type= radio name=question7 value= wrong> Answer3");
-  $(".Questions").append("<input type= radio name=question7 value= wrong> Answer4");
+  $(".Questions").append("<h1 class = questionheader>What joint U.S.-Canadian government entity is charged with tracking Santa every Christmas?  </h1>");
 
-  $(".Questions").append("<h1 class = questionheader> QUESTION 8 QUESTION 8 QUESTION 8 QUESTION 8 </h1>");
+  $(".Questions").append("<input type= radio name=question6 value= correct> NORAD");
+  $(".Questions").append("<input type= radio name=question6 value= wrong> NOAH");
+  $(".Questions").append("<input type= radio name=question6 value= wrong> XMAS UNIT");
+  $(".Questions").append("<input type= radio name=question6 value= wrong> CAD");
 
-  $(".Questions").append("<input type= radio name=question8 value= wrong> Answer1");
-  $(".Questions").append("<input type= radio name=question8 value= wrong> Answer2");
-  $(".Questions").append("<input type= radio name=question8 value= wrong> Answer3");
-  $(".Questions").append("<input type= radio name=question8 value= correct> Correct <br>");
+  $(".Questions").append("<h1 class = questionheader> Every year a tree is decorated in Trafalgar Square in London. Which nation donates that tree? </h1>");
+
+  $(".Questions").append("<input type= radio name=question7 value= correct> Norway");
+  $(".Questions").append("<input type= radio name=question7 value= wrong> Canada");
+  $(".Questions").append("<input type= radio name=question7 value= wrong> Denmark");
+  $(".Questions").append("<input type= radio name=question7 value= wrong> Sweden");
+
+  $(".Questions").append("<h1 class = questionheader> What evil Santa counterpart kidnaps naughty children in Germany? </h1>");
+
+  $(".Questions").append("<input type= radio name=question8 value= wrong> Frampus");
+  $(".Questions").append("<input type= radio name=question8 value= wrong> The Bleeken");
+  $(".Questions").append("<input type= radio name=question8 value= wrong> Sweeney Todd");
+  $(".Questions").append("<input type= radio name=question8 value= correct> Krampus<br>");
   $(".Questions").append("<pre id=log></pre>");
 
 
@@ -105,20 +105,13 @@ function decrement (){ //counts down and resets when it hits 0
   $(".countdown").html("<h2>" + number + "</h2>")
   if (number == 0){
     handleClick();
-    // printresults();
     stop();
-    // $(".Questions").empty();
-    // $(".Questionscontainer").empty();
-    // document.getElementById('startbutton').style.display="inline";
-    // printresults();
+
   }
 }
 
 function submit(){ //runs the handleClick when button is pressed
-
   $("#donebutton").on("click", stop);
-
-
 }
 
 
@@ -152,28 +145,18 @@ function handleClick(){ //loops through all input answers, tallys incorrect, cor
   } if (unanswered === -1){
     unanswered = 0;
   }
+}
 
-  // alert("Correct Answers: " + correctanswers + ". Wrong Answers: " + wronganswers +". Unanswered: " + unanswered); // alerts the results because I can't print them to the DOM for some reason
-
+function printresults (){ //pushes results to DOM
+    $(".Questionscontainer").append("<div class = Results>");
+    $(".Results").append("<p> Correct Answers: " + correctanswers);
+    $(".Results").append("<p> Wrong Answers: " + wronganswers);
+    $(".Results").append("<p> Unanswered Questions: " + unanswered);
+    $(".Results").append("</div>");
 }
 
 
-
-function printresults (){ //why doesn't this work?
-    console.log ("init");
-    $(".Questionscontainer").append("Correct Answers: " + correctanswers);
-    $(".Questionscontainer").append("Wrong Answers: " + wronganswers);
-    $(".Questionscontainer").append("Unanswered Questions: " + unanswered);
-}
-
-
-
-// function stopgame () { //stops the game when button is pressed
-//   $("#donebutton").on("click", stop);
-// }
 
 
 //calls
 game();
-// submit();
-// stopgame();
